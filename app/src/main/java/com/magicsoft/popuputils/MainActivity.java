71202d393a6f04fca358547c8a1343887140bb2b
kiwilss <m15205622327@163.com>
2018-03-19 10:45:01 +0800

    3.19-新增常见修改头像对话框

--- app/src/main/java/com/magicsoft/popuputils/MainActivity.java
+++ app/src/main/java/com/magicsoft/popuputils/MainActivity.java
@@ -5,9 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
-import com.magicsoft.mylibrary.PopupOneBtn;
-import com.magicsoft.mylibrary.PopupOneUtils;
 import com.magicsoft.mylibrary.PopupWindowUtils;
+import com.magicsoft.mylibrary.PwChoiceHeader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,42 +21,55 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void oneTitleOneBtn(View view) {
-        new PopupOneBtn(this, new PopupOneBtn.BtnClick() {
+        PwChoiceHeader pwChoiceHeader = new PwChoiceHeader(this, new PwChoiceHeader.ChoiceHeader() {
             @Override
-            public void sureClickListener(PopupOneBtn popupOneBtn) {
-                Toast.makeText(MainActivity.this, "确定", Toast.LENGTH_SHORT).show();
+            public void takePhoto(PwChoiceHeader pwChoiceHeader) {
+                Toast.makeText(MainActivity.this, "takephoto", Toast.LENGTH_SHORT).show();
             }
-        }).showCenter(this);
+
+            @Override
+            public void openAlbum(PwChoiceHeader pwChoiceHeader) {
+                Toast.makeText(MainActivity.this, "openalbum", Toast.LENGTH_SHORT).show();
+            }
+        });
+        pwChoiceHeader.setAnimationStyle(R.style.PushInBottom);
+        pwChoiceHeader.showBottom(this);
+//        new PopupOneBtn(this, new PopupOneBtn.BtnClick() {
+//            @Override
+//            public void sureClickListener(PopupOneBtn popupOneBtn) {
+//                Toast.makeText(MainActivity.this, "确定", Toast.LENGTH_SHORT).show();
+//            }
+//        }).showCenter(this);
     }
 
     public void oneTitleOneBtnModify(View view) {
-        PopupOneBtn pob = new PopupOneBtn(this, new PopupOneBtn.BtnClick() {
-            @Override
-            public void sureClickListener(PopupOneBtn popupOneBtn) {
-                Toast.makeText(MainActivity.this, "确定", Toast.LENGTH_SHORT).show();
-            }
-        });
-        pob.setTitleAndColor("任意标题",0,false)
-                .setSureTextAndColor("任意文字",
-                        R.color.colorPrimary,R.color.colorAccent)
-                .setRadius(2)//设置圆角
-                .showCenter(this);
+//        PopupOneBtn pob = new PopupOneBtn(this, new PopupOneBtn.BtnClick() {
+//            @Override
+//            public void sureClickListener(PopupOneBtn popupOneBtn) {
+//                Toast.makeText(MainActivity.this, "确定", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        pob.setTitleAndColor("任意标题",0,false)
+//                .setSureTextAndColor("任意文字",
+//                        R.color.colorPrimary,R.color.colorAccent)
+//                .setRadius(2)//设置圆角
+//                .showCenter(this);
     }
 
     public void menu(View view) {
-        PopupOneUtils popupOneUtils = new PopupOneUtils(this, new PopupOneUtils.ContentClickListener() {
-            @Override
-            public void sureClickListener(PopupOneUtils popupOneUtils) {
-
-            }
-
-            @Override
-            public void cancelClickListener() {
-
-            }
-        });
-        popupOneUtils.setAnimationStyle(R.style.PopDownLeftMenu);
-        popupOneUtils.showAsDropDown(view);
+//        PopupOneUtils popupOneUtils = new PopupOneUtils(this, new PopupOneUtils.ContentClickListener() {
+//            @Override
+//            public void sureClickListener(PopupOneUtils popupOneUtils) {
+//
+//            }
+//
+//            @Override
+//            public void cancelClickListener() {
+//
+//            }
+//        });
+//        popupOneUtils.setAnimationStyle(R.style.PopDownLeftMenu);
+//        popupOneUtils.showAsDropDown(view);
         //popupOneUtils.showAsDropDown(view,0,0);
     }
 
